1772da581406361d43c172b6f220767001675bdc
R. Martell <rjmartell@computer.org>
2016-10-14 14:37:12 -0700

    Implement test method QueryableWMS::invokeBaseTestSuite()

--- ets-dgiwg-wms/src/main/java/de/latlon/ets/wms13/core/dgiwg/testsuite/QueryableWMS.java
+++ ets-dgiwg-wms/src/main/java/de/latlon/ets/wms13/core/dgiwg/testsuite/QueryableWMS.java
@@ -1,10 +1,20 @@
 package de.latlon.ets.wms13.core.dgiwg.testsuite;
 
+import static org.testng.Assert.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.xml.transform.Source;
@@ -21,10 +31,14 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 
 import de.latlon.ets.core.util.XMLUtils;
 import de.latlon.ets.wms13.core.TestRunArg;
+import net.sf.saxon.s9api.Axis;
+import net.sf.saxon.s9api.QName;
 import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XdmNode;
 import net.sf.saxon.s9api.XdmValue;
 import net.sf.saxon.trans.XPathException;
 
@@ -50,10 +64,15 @@ import net.sf.saxon.trans.XPathException;
 public class QueryableWMS {
 
     private static final Logger LOGR = Logger.getLogger(QueryableWMS.class.getPackage().getName());
+    private Level logLevel = Level.INFO;
     /** Location of default test execution service (OGC beta installation). */
     private static final URI DEFAULT_TES = URI.create("http://cite.opengeospatial.org/te2/");
     /** HTML namespace name. */
     private static final String HTML_NS = "http://www.w3.org/1999/xhtml";
+    /** Test run parameter: Location of capabilities document (URI). */
+    private static final String CAPABILITIES_URL = "capabilities-url";
+    /** Test run parameter: Run tests for 'Queryable WMS' (boolean). */
+    private static final String QUERYABLE = "queryable";
     /** JAX-RS Client component. */
     private Client httpClient;
     /** Endpoint of base WMS test run controller. */
@@ -104,20 +123,70 @@ public class QueryableWMS {
                 throw new SkipException(e.getMessage());
             }
         }
-        LOGR.info(this.baseTestRunController.toString());
+        LOGR.log(logLevel, this.baseTestRunController.toString());
     }
 
     /**
-     * Invokes the OGC WMS test suite. The following test run arguments are set:
+     * Runs the OGC WMS test suite by submitting a (GET) request to the test run
+     * controller. The following test run arguments are set:
      * <ul>
-     * <li>capabilities-url: A URI that refers to a WMS capabilities
+     * <li>capabilities-url : A URI that refers to a WMS capabilities
      * document</li>
      * <li>queryable: "queryable"</li>
      * </ul>
+     * 
+     * @throws IOException
+     *             If an I/O error occurs while trying to read or write the test
+     *             run results.
      */
     @Test(description = "See DGIWG-112: Table 1, Requirement 1")
-    public void invokeBaseTestSuite() {
+    public void invokeBaseTestSuite() throws IOException {
+        LOGR.log(logLevel, "Starting base test run at {0}", this.baseTestRunController);
+        MultivaluedMap<String, String> args = new MultivaluedMapImpl();
+        args.putSingle(CAPABILITIES_URL, this.wmsCapabilitiesRef);
+        args.putSingle(QUERYABLE, QUERYABLE);
+        WebResource resource = this.httpClient.resource(this.baseTestRunController).queryParams(args);
+        ClientResponse rsp = resource.accept(MediaType.APPLICATION_XML).get(ClientResponse.class);
+        assertEquals(rsp.getStatus(), ClientResponse.Status.OK.getStatusCode(), "Unexpected status code.");
+        LOGR.log(logLevel, "Test run was completed.");
+        assertTrue(rsp.hasEntity(), "No entity in response from test run controller.");
+        File results = saveEntityToFile(rsp);
+        LOGR.log(logLevel, "Test run results were saved to {0}", results);
+        // examine result of main test
+        XdmNode mainTestNode;
+        try {
+            XdmValue xdmVal = XMLUtils.evaluateXPath2(new StreamSource(results), "/execution/log[1]", null);
+            mainTestNode = (XdmNode) xdmVal.itemAt(0);
+        } catch (SaxonApiException e) {
+            throw new AssertionError(e.getMessage());
+        }
+        XdmNode startTest = (XdmNode) mainTestNode.axisIterator(Axis.CHILD, new QName("starttest")).next();
+        LOGR.log(logLevel, "Base test run identifier: {0}", startTest.getAttributeValue(new QName("path")));
+        XdmNode endTest = (XdmNode) mainTestNode.axisIterator(Axis.CHILD, new QName("endtest")).next();
+        int statusCode = Integer.parseInt(endTest.getAttributeValue(new QName("result")));
+        TestVerdict mainVerdict = TestVerdict.values()[statusCode - 1];
+        assertEquals(mainVerdict, TestVerdict.PASS, "Unexpected verdict for main test.");
+    }
 
+    /**
+     * Writes the content of the response entity to a temporary file.
+     * 
+     * @param rsp
+     *            An HTTP response message.
+     * @return A File containing the entity body.
+     * @throws IOException
+     *             If an I/O error occurs.
+     */
+    File saveEntityToFile(ClientResponse rsp) throws IOException {
+        File destFile = File.createTempFile("wmsResults-", ".xml");
+        try (InputStream in = rsp.getEntityInputStream(); OutputStream out = new FileOutputStream(destFile)) {
+            byte[] buffer = new byte[8 * 1024];
+            int bytesRead;
+            while ((bytesRead = in.read(buffer)) != -1) {
+                out.write(buffer, 0, bytesRead);
+            }
+        }
+        return destFile;
     }
 
     /**
@@ -164,5 +233,15 @@ public class QueryableWMS {
             // not possible with atomic value
         }
         return controllerUri;
+    }
+
+    /**
+     * Test verdicts assigned in CTL test suites. For each verdict, its
+     * corresponding status code is given by ordinal() + 1. Conversely, to look
+     * up a verdict by status code, evaluate TestVerdict.values()[statusCode -
+     * 1].
+     */
+    enum TestVerdict {
+        PASS, NOT_TESTED, SKIP, WARNING, INHERITED_FAILURE, FAIL;
     }
 }
